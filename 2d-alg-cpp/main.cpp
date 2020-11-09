410e7047e3ef781a21acee101b4239275ca8897e
Edward Zelis <zeledvards@gmail.com>
2020-11-09 08:52:48 +0200

    slick

--- 2d-alg-cpp/main.cpp
+++ 2d-alg-cpp/main.cpp
@@ -17,19 +17,14 @@ int main()
     std::sort(words.begin(), words.end());
 
     for (int i = 0; i < words.size() - 1; i++)
-    {
         if (words.at(i) != words.at(i + 1))
-        {
             indexes.push_back(i + 1);
-        }
-    }
 
     indexes.insert(indexes.begin(), 0);
     indexes.push_back(words.size());
 
     for (int i = 0; i < indexes.size() - 1; i++)
-    {
         std::cout << words.at(indexes.at(i)) << " " << indexes.at(i + 1) - indexes.at(i) << "\n";
-    }
+
     return EXIT_SUCCESS;
 }
